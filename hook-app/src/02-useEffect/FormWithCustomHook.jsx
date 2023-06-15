b10a25d7208073824eeb9104d3ef6c836dac71bd
pauloosilas <paulo3356@gmail.com>
2023-06-15 19:32:47 -0300

    Form With Custom hook

---
+++ hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,52 @@
+import { useForm } from "../hooks/useForm";
+import { Message } from "./Message";
+
+export const FormWithCustomHook = () =>{
+
+    const {formState, onInputChange, username, email, password} = useForm( {
+        username: '',
+        email:'',
+        password: ''
+    })
+
+   // const { username, email, password } = formState;   
+
+
+    return(
+        <>
+            <h1>Formulario com Custom Hook</h1>
+            <hr />
+
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value = {username}
+                onChange={onInputChange}
+            />
+
+            <input 
+                type="email"
+                className="form-control mt-2"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+
+            />
+            <input 
+                type="password"
+                className="form-control mt-2"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+
+            />
+            {
+                (username === 'paulo') &&  <Message />
+            }
+        </>
+    )
+}
