1f4b6a92f926b6c2062ba0acab04ad03bc3cb82e
pauloosilas <paulo3356@gmail.com>
2023-06-16 19:13:34 -0300

    hook useRef

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -5,12 +5,13 @@ import ReactDOM from 'react-dom/client'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx'
 //import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
+import { FocusScreen } from './04-useRef/FocusScreem.jsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
  </React.StrictMode>,
 )
