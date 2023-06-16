3189c7ac2266a21e570bcb960e82f8518f6de1a5
pauloosilas <paulo3356@gmail.com>
2023-06-16 16:35:31 -0300

    useFetch + useCounter

--- hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -6,10 +6,33 @@ export const MultipleCustomHooks = () => {
 
     console.log(data, isLoading, hasError)
 
+   const { author, quote} = !!data && data[0]
+
     return(
         <>
             <h1>Breaking Bad</h1>
             <hr />
+            
+            {
+              isLoading
+                ? (
+                    <div className="alert alert-info text-center">
+                        Loding...
+                    </div>
+                )
+                :(
+                <blockquote className="blockquote text-end">
+                    <p className="mb-1">{ quote }</p>
+                    <footer className="blockquote-footer">{ author }</footer>
+                </blockquote>
+
+            )
+    }
+
+        <button className="btn btn-primary">
+            Next Quote
+        </button>
+
         </>
     )
 }
